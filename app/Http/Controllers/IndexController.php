4eb47aca9fb320b77fd65fbdd87d77a458ea6dad
Andrei Timotin <andreitimotin11@gmail.com>
2017-03-19 22:21:59 +0200

    view

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,6 +7,7 @@ use App\Page;
 use App\Service;
 use App\Portfolio;
 use App\People;
+use DB;
 
 use App\Http\Requests;
 
@@ -17,7 +18,8 @@ class IndexController extends Controller
         $pages = Page::all();
         $portfolios = Portfolio::get(array('name','filter','images'));
         $services = Service::where('id','<',20)->get();
-        $peoples = People::take(3)->get();
+        $peoples = People::take(3)->get();.
+	    $tags = DB::table('portfolios')->distinct()->lists('filter');dd($tags);
 //        dump($pages);
 //        dump($portfolios);
 //        dump($services);
@@ -34,6 +36,7 @@ class IndexController extends Controller
         $item = array('title'=>'Team', 'alias'=> 'team');
         array_push($menu, $item);
         $item = array('title'=>'Contact', 'alias'=> 'contact');
+        array_push($menu, $item);$item = array('title'=>'Contact', 'alias'=> 'contact');
         array_push($menu, $item);
         return view('site.index', array(
             'menu' => $menu,
