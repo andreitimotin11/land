06268f0af3871182f7bfc9e0b86ebfbd4a916d9c
Andrei Timotin <andreitimotin11@gmail.com>
2017-03-19 18:26:47 +0200

    add migrations

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Page;
+use App\Service;
+use App\Portfolio;
+use App\People;
 
 use App\Http\Requests;
 
@@ -10,6 +14,15 @@ class IndexController extends Controller
 {
     public function execute(Request $request)
     {
+        $pages = Page::all();
+        $portfolios = Portfolio::get(array('name','filter','images'));
+        $services = Service::where('id','<',20)->get();
+        $peoples = People::take(3)->get();
+        dump($pages);
+        dump($portfolios);
+        dump($services);
+        dump($peoples);
+
         return view('layouts.site');
     }
 }
